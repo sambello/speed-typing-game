6f28434db75e8282d15412981f694dfd22de07cd
Samuel Bello <sambello25@gmail.com>
2020-09-30 11:09:14 -0400

    Updates

---
+++ precache-manifest.1f0faf5b0557633e40c3470b8765ff6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536c3447c893dabf880ebf5274404e31",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "ca86d7da03510040282e",
+    "url": "/speed-typing-game/static/css/main.8fe996c1.chunk.css"
+  },
+  {
+    "revision": "18c228c764d753be372e",
+    "url": "/speed-typing-game/static/js/2.e54ca885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.e54ca885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca86d7da03510040282e",
+    "url": "/speed-typing-game/static/js/main.7bfaece5.chunk.js"
+  },
+  {
+    "revision": "294e6ac9cd4f7e7f2592",
+    "url": "/speed-typing-game/static/js/runtime-main.b1de8fe3.js"
+  }
+]);
